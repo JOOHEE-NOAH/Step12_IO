db7bc469cb13b3d42fcc0a949edd0dea7b52800c
JOOHEE-NOAH <joohee195@gmail.com>
2022-02-15 09:28:28 +0900

    해설추가

--- src/test1/ScoreTest.java
+++ src/test1/ScoreTest.java
@@ -10,43 +10,39 @@ import java.io.InputStreamReader;
 public class ScoreTest {
 
 	public static void main(String[] args) throws IOException {	
-		//한줄단위로 읽는 애 사용해야함.
-//		InputStreamReader isr = new FileReader("src\\test1\\score.txt");
-//		BufferedReader br = new BufferedReader(isr);
+		//문자단위로 읽을 수 있느 버퍼드 리더사용
+		BufferedReader br = new BufferedReader(new FileReader("src/test1/score.txt")); //파일 리더: 직접 경로를 지정해 찾기
+		
+		//줄바꿈이 일어나는 시점에
+		String name;
+		int k, e, m;
+		
+		while(true) {	//반복루프
+				name=br.readLine();
+				if(name==null)
+					break;
+				k=Integer.parseInt(br.readLine()); //문자열을 숫자로 처리
+				e=Integer.parseInt(br.readLine());
+				m=Integer.parseInt(br.readLine());// br.readline하면 말그대로 줄마다 읽음..;;;;
+				
+				System.out.print("이름 : "+name);
+				System.out.print("\t총점 : "+(k+e+m));
+				System.out.printf("\t평균 : %.2f\n",(double)(k+e+m)/3);
+		}
+	
+		//==============풀이 1================
+//		while((name=br.readLine())!=null) {	//반복루프
+//		k=Integer.parseInt(br.readLine()); //문자열을 숫자로 처리
+//		e=Integer.parseInt(br.readLine());
+//		m=Integer.parseInt(br.readLine());
 //		
-//		String name=br.readLine();
-//		int kor=Integer.parseInt(name);
-//		int eng=Integer.parseInt(name);
-//		int mat=Integer.parseInt(name);
-//		double tot=(double)(kor+eng+mat);
-//		double avg= tot/3.0;
-//		
-//		while(true) {
-//			if((name==null)){
-//			
-//		System.out.println(name);
-//			}
-//			
-//		}
-//		
-		 File file=new File("src\\test1\\score.txt");
-	      BufferedInputStream bis=new BufferedInputStream(new FileInputStream(file));
-	      
-	      String str;
-	      int kor,eng,mat;
-	      byte[] buffer=new byte[(int) file.length()];
-	      
-	      bis.read(buffer, 0, 6);
-	      bis.read(buffer, 6, 6);
-	      bis.read(buffer, 7, 7);
-	      
-//	      bis.read(buffer, 8, 8);
-	      
-//	      bis.read(buffer, 0, buffer.length);
-	      System.out.println(new String(buffer));
-//	      System.out.println((a+b+c)/2);
-	      
-	      bis.close();
+//		System.out.print("이름 : "+name);
+//		System.out.print("\t총점 : "+(k+e+m));
+//		System.out.printf("\t평균 : %.2f\n",(double)(k+e+m)/3);
+//	
+	
+
+		
 
 	}
 
