f08ac6357676dfd7a5610999a772e6d6cdff5dde
JOOHEE-NOAH <joohee195@gmail.com>
2022-02-15 16:26:56 +0900

    my solution

---
+++ src/test4/User.java
@@ -0,0 +1,32 @@
+package test4;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+	
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String name;
+	private int age;
+	private double tall;
+	
+	public User() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public User(String name, int age, double tall) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.tall = tall;
+	}
+	
+	public String toString() {
+		return "이름:"+name+" 나이:"+age+" 신장: "+tall+"\n";
+	}
+
+
+}
